954ce62850cdeac983f201bf5dc3873aefc077d1
kamin0 <jm187@yandex.ru>
2022-10-11 23:43:47 +0300

    Добавьте файлы проекта.

---
+++ Final15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var classes = new[]
+        {
+           new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+           new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+           new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+       };
+        var allStudents = GetAllStudents(classes);
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        var allStudents = classes.SelectMany(x => x.Students.Select(s => s)).ToArray();
+        return allStudents;
+    }
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
